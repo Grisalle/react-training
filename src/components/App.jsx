5ba9eb0add8479694776b67792fed7ce771cf853
Yurii Hrymchak <130244267+Grisalle@users.noreply.github.com>
2023-11-16 22:40:57 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,7 @@
-import { Routes, Route, NavLink } from 'react-router-dom';
-import styled from 'styled-components';
-import Home from 'path/to/pages/Home';
-import About from 'path/to/pages/About';
-import Products from 'path/to/pages/Products';
-
-const StyledLink = styled(NavLink)`
-  color: black;
-
-  &.active {
-    color: orange;
-  }
-`;
-
 export const App = () => {
   return (
     <div>
-      <nav>
-        <StyledLink to="/" end>
-          Home
-        </StyledLink>
-        <StyledLink to="/about">About</StyledLink>
-        <StyledLink to="/products">Products</StyledLink>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-      </Routes>
+      <h1>React</h1>
     </div>
   );
 };
